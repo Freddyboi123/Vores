6445a533824071b000bf8cc752d57fc36a8a1c90
Freddyboi123 <Frededv@outlook.dk>
2026-03-20 10:29:34 +0100

    cleanup

--- src/main/java/app/config/Hibernate/HibernateBaseProperties.java
+++ src/main/java/app/config/Hibernate/HibernateBaseProperties.java
@@ -1,4 +1,4 @@
-package app.config;
+package app.config.Hibernate;
 
 import java.util.Properties;
 
---
+++ src/main/java/app/devTools/Populator.java
@@ -0,0 +1,52 @@
+package app.devTools;
+
+import app.entities.Weather.WeeklyForcast;
+import app.utils.WeatherApiHandler.WeatherApi;
+
+public class Populator {
+
+    public static void main(String[] args) {
+//        UserDAO userDAO = new UserDAO(emf);
+//
+//        User user = User.builder().name("Frederik").email("frederik@gmail.com").password("12345").build();
+//        User user2 = User.builder().name("Emil").email("Emil@gmail.com").password("12345").build();
+//
+//        PrivacySettings privacySettings = new PrivacySettings(true,true,false);
+//        PrivacySettings privacySettings2 = new PrivacySettings(true,true,false);
+//
+//        user.setPrivacySettings(privacySettings);
+//        user2.setPrivacySettings(privacySettings2);
+//
+//        userDAO.createUser(user);
+//        userDAO.createUser(user2);
+//
+//        Post post = Post.builder().postContent("This is a test post").user(user).build();
+//        Post post2 = Post.builder().postContent("This is alo a test, and will be removed").user(user2).build();
+//
+//        user.addPost(post);
+//        user2.addPost(post2);
+//
+//        PostDAO postDAO = new PostDAO(emf);
+//        postDAO.createPost(post);
+//        postDAO.createPost(post2);
+//
+//        Comment comment1 = new Comment("hello this is a test",user,post);
+//        Comment comment2 = new Comment("hello this is a test",user,post2);
+//        Comment comment3 = new Comment("hello this is a test2",user2,post2);
+//        user.addComment(comment1);
+//        user.addComment(comment2);
+//        user2.addComment(comment3);
+//
+//
+//        CommentDAO commentDAO = new CommentDAO(emf);
+//        commentDAO.createComment(comment1);
+//        commentDAO.createComment(comment2);
+//        commentDAO.createComment(comment3);
+//
+//        userDAO.deleteUser(user2.getId());
+
+        WeatherApi weatherApi = new WeatherApi();
+        WeeklyForcast forecast = weatherApi.getWeatherByCity("Højby", "Odsherred Kommune");
+        forecast.getWeeklyForecast();
+    }
+}
