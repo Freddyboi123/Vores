f8d213c177062f8f1fafb1a47f7fc13ce114f952
Freddyboi123 <Frededv@outlook.dk>
2026-04-07 23:34:52 +0200

    dev tool implemented

--- src/main/java/app/rest/Routes.java
+++ src/main/java/app/rest/Routes.java
@@ -33,7 +33,7 @@ public class Routes {
         return switch (resourceName.toLowerCase()) {
             case "msg" -> () -> path("msg", () -> {
                 ObjectNode on = objectMapper.createObjectNode();
-                on.put("msg", "Hello World from your new and correct Java Project, now with WatchTower. WUHU watchtower is working!, nowtissemand");
+                on.put("msg", "Hello World from your new and correct Java Project, now with WatchTower. WUHU watchtower is working!");
                 get("hello", ctx -> ctx.json(on));
                 post("echo", ctx -> ctx.result(ctx.body()));
             });
