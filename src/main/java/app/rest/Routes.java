a777abe022fbd4b4e01cdd79367272fd9a37ed9a
Freddyboi123 <Frededv@outlook.dk>
2026-04-10 21:56:30 +0200

    final push comits

--- src/main/java/app/rest/Routes.java
+++ src/main/java/app/rest/Routes.java
@@ -36,7 +36,7 @@ public class Routes {
         return switch (resourceName.toLowerCase()) {
             case "msg" -> () -> path("msg", () -> {
                 ObjectNode on = objectMapper.createObjectNode();
-                on.put("msg", "Hello World from your new and correct Java Project, now with WatchTower. WUHU watchtower is working!");
+                on.put("msg", "Hello and welcome to my exam project");
                 get("hello", ctx -> ctx.json(on));
                 post("echo", ctx -> ctx.result(ctx.body()));
             });
